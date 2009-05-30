7e43fcfe53f37d1f937e656bd13d8d6a5fcc90d6
joshgeenen <joshgeenen>
2009-05-30 18:23:43 +0000

    Bumped version up to 0.2.4a1

--- src2/content/synonyms.js
+++ src2/content/synonyms.js
@@ -45,7 +45,7 @@ var Cr = Components.results;
 
 var nsIAbCard      = Ci.nsIAbCard;
 var dummyEmailName = "PrimaryEmail";
-var version        = "0.2.3";
+var version        = "0.2.4a1";
 
 function serialize(aXML, aRemoveVersion) {
   if (!aXML)
