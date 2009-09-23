8134c307f4e8edf36ebaefe871b0ee7bd7ac432f
joshgeenen <joshgeenen>
2009-09-23 01:17:21 +0000

    Bumped up the version to 0.2.8 and fixed the install.rdf file

--- src2/content/synonyms.js
+++ src2/content/synonyms.js
@@ -45,7 +45,7 @@ var Cr = Components.results;
 
 var nsIAbCard      = Ci.nsIAbCard;
 var dummyEmailName = "PrimaryEmail";
-var version        = "0.2.7";
+var version        = "0.2.8";
 
 function serialize(aXML, aRemoveVersion) {
   if (!aXML)
