cf709360b35150269574210a70b4d9d430e7b5f0
joshgeenen <joshgeenen>
2010-05-05 17:07:29 +0000

    Added a basic build script

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# gContactSync build script
+# This script builds gContactSync into an xpi that may be installed in
+# compatible applications (Thunderbird, Seamonkey, and Postbox).
+
+# Start editing here
+
+# NOTE: The VERSION here is just used for the package name
+# Edit install.rdf and content/synonyms.js to change the actual VERSION
+VERSION=0.2.16
+SRC_DIR=./src2
+# Dest should be absolute or relative to SRC_DIR
+DEST=$(pwd)/downloads/gContactSync-$VERSION.xpi
+
+# Stop editing here
+
+# remove the existing zip file
+if [ -f $DEST ]; then
+  echo "Removing previous zip file at:"
+  echo $DEST
+  rm -f $DEST
+fi
+
+cd $SRC_DIR
+
+# finds all files in locale/ excluding CVS directories and CVS files
+LOCALE_FILES=$(find locale/ -maxdepth 2 -type f ! -name CVS ! -name Repository ! -name Root ! -name Entries)
+
+# zip the source files
+zip -r $DEST content/*.* defaults/preferences/*.* $LOCALE_FILES skin/*.* install.rdf chrome.manifest
+
+# quit if zip failed
+if [ "$?" != 0 ]; then
+  echo "ERROR: zip failed, check '$DEST'"
+  exit 1
+fi
+
+# remove CVS folders
+#zip -d $DEST CVS/* */CVS/* */*/CVS/*
+
+echo "Package ready at:"
+echo $DEST
+echo "MD5 checksum:"
+md5sum $DEST
+
