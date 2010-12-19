453bcc9946521f31576b00350603e96e29d00032
joshgeenen <joshgeenen>
2010-12-19 23:51:02 +0000

    Bug 23550 - Replace 'Thunderbird' with the actual user agent, if applicable - r=joshgeenen

--- src3/content/StringBundle.js
+++ src3/content/StringBundle.js
@@ -50,6 +50,11 @@ com.gContactSync.StringBundle = {
   mBundles:     {},
   /** Stores whether this class has been initialized */
   mInitialized: false,
+  /** Stores whether the user agent should be replaced in strings */
+  mReplaceUserAgent: false,
+  /** The user agent (Thunderbird, SeaMonkey, or Postbox). */
+  mUserAgent:   "Thunderbird",
+
   /**
    * Initializes the string bundle.
    */
@@ -57,34 +62,59 @@ com.gContactSync.StringBundle = {
     if (com.gContactSync.StringBundle.mInitialized) {
       return true;
     }
+
     com.gContactSync.StringBundle.mBundles.mStrings =
       document.getElementById("gContactSyncStringBundle");
+      
     if (!com.gContactSync.StringBundle.mBundles.mStrings) {
       var err = "Error - com.gContactSync.StringBundle could not be initialized\n";
       com.gContactSync.alertError(err);
       throw err;
     }
+
+    // Check the user agent
+    if (window.navigator.userAgent.indexOf("SeaMonkey") != -1) {
+      com.gContactSync.StringBundle.mUserAgent        = "SeaMonkey";
+      com.gContactSync.StringBundle.mReplaceUserAgent = true;
+    } else if (window.navigator.userAgent.indexOf("Postbox") != -1) {
+      com.gContactSync.StringBundle.mUserAgent        = "Postbox";
+      com.gContactSync.StringBundle.mReplaceUserAgent = true;
+    }
+
     com.gContactSync.StringBundle.mInitialized = true;
     return true;
   },
   /**
    * Searches every string bundle until a string is found with the given name.
+   * If com.gContactSync.StringBundle.mReplaceUserAgent is set to true, this
+   * will replace all instances of "Thunderbird" with the string in
+   * com.gContactSync.StringBundle.mUserAgent.
    * @param aName {string} The name of the string to search for.
    * @returns {string} The translated string.
    */
   getStr: function StringBundle_getStr(aName) {
     var str = aName, i;
+    
     // initialize the string bundle if it wasn't already done
     if (!com.gContactSync.StringBundle.mInitialized) {
       com.gContactSync.StringBundle.init();
     }
+
     for (i in com.gContactSync.StringBundle.mBundles) {
       try {
         str = com.gContactSync.StringBundle.mBundles[i].getString(aName);
+
+        // If necessary, replace all instances of Thunderbird with the actual
+        // user agent.
+        if (com.gContactSync.StringBundle.mReplaceUserAgent) {
+          str = str.replace(/Thunderbird/g, com.gContactSync.StringBundle.mUserAgent);
+        }
+
         break;
       } catch (e) {} // if the bundle doesn't exist or if the string
                      // isn't in it skip to the next
     }
+
     return str;
   }
 };
