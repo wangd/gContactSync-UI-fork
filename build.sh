43691a515af6186ed25c7e4fbc824e19553cfb34
Daniel L. Wang <wangd@uci.edu>
2011-01-20 17:06:12 -0800

    Fix build script to append git hash to version number (merge)
    Conflicts:
    
    	build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,10 @@
 #SRC_DIR=./src2
 #VERSION=0.3.5
 #SRC_DIR=./src3
-VERSION=0.4.0a4
-SRC_DIR=./src4
+VERSION=0.3.0_`git show -s --format=%h`
+SRC_DIR=./src3
+#VERSION=0.4.0a4
+#SRC_DIR=./src4
 
 # Dest should be absolute or relative to SRC_DIR
 DEST=$(pwd)/downloads/gContactSync-$VERSION.xpi
